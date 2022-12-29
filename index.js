7b5518153dd674bd1b6016fa1c967807b8260285
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-12-29 13:36:14 +0600

    User role API Get

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('ShowFeed server is running');
-});
 
 
 
@@ -20,23 +17,47 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const postsCollection = client.db('showfeed').collection('posts');
+        const usersCollection = client.db('showfeed').collection('users');
 
         app.get('/posts', async (req, res) => {
             const posts = {};
             const result = await postsCollection.find(posts).toArray();
             res.send(result);
+        });
+
+        app.post('/post', async (req, res) => {
+            const posts = req.body;
+            const result = await postsCollection.insertOne(posts);
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            console.log(users)
+            const result = await usersCollection.insertOne(users);
+            res.send(result);
+        });
+
+        app.get('/users/sfUser/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isUser: user.role === 'sfUser' });
         })
 
     }
     finally { }
 }
 
-run().catch(err => console.log(err))
+run()
+    .catch(err => console.log(err))
 
 
 
 
-
+app.get('/', (req, res) => {
+    res.send('ShowFeed server is running');
+});
 
 
 app.listen(PORT, () => {
